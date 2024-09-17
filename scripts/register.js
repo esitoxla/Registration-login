7aa54141a04098ccadc9e358a27b04de6b8ad6e3
Irene Toxla <toxlairene@gmail.com>
2024-09-17 15:57:30 +0000

    updated register.js

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,28 @@
 // get form element by id
-const registerForm = document. getElementById( 'register');
+const registerForm = document.querySelector('#register');
 // handle form submit event
-registerForm.onsubmit =function(event){
+registerForm.addEventListener ('submit', function (event){ 
     event.preventDefault();
- console.log(event)
+     //collect form data
+     const formData = new FormData(registerForm);
+     //send data to backend
+     //display success message to user
+     const messageH1 = document.querySelector('#message');
+     messageH1.textContent = 'Account registered successfully!';
+
+});
+//declare variable age
+const age = 71;
+
+
+//define squareAge function
+function squareAge (age) {
+    return age**2;
 }
+
+const squaredAge = squareAge(34);
+console.log (squaredAge);
+
+
+
+
